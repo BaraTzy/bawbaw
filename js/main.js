315e9cdeb1a41aa30ea774c8c8c7efa729be9dbb
BaraTzy <anakkampungofficial2@gmail.com>
2025-02-14 00:48:00 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('YOU ARE THE LOVE OF MY LIFE').split('')
+    const titles = ('I LOVE U PUSPITAKU').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
