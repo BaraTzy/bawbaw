0e10e632a5602c10bdd4419645cba8936569a8cd
BaraTzy <anakkampungofficial2@gmail.com>
2025-09-18 02:10:12 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'Happy Valentine Day'.split('')
+const text = 'Get Well Soon akak :)'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
