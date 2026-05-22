7eb49f18ca018b08759f570c86151dcb2104d906
BaraTzy <anakkampungofficial2@gmail.com>
2026-05-22 18:43:18 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'BUAT PRESIDEN MUNGIL'.split('')
+const text = 'BUAT KAKA TERSAYANG'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
